968507eade87628953789eb129f9a53e3d0b1a12
sasithhansaka <sasithhansaka285@gmail.com>
2024-12-12 01:28:06 +0530

    Fotter component ,showallcardevents components added

---
+++ TicketTales/src/Components/ShowCardAllEvents.jsx
@@ -0,0 +1,152 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./ShowCard.css";
+import { useNavigate } from "react-router-dom";
+
+function ShowCardAllEvents({ filter }) {
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/AllEvents");
+        setEvents(response.data);
+      } catch (error) {
+        console.error("Error fetching data", error);
+        setError("Failed to load data");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  if (loading) return <p>Loading events...</p>;
+  if (error) return <p>{error}</p>;
+
+  const currentMonth = new Date()
+    .toLocaleString("default", { month: "long" })
+    .toUpperCase();
+
+  const GotoTicketBuy = (eventid) => {
+    console.log("click");
+    navigate("/PurchaseTicket", { state: { eventid } });
+  };
+
+  const filteredEvents = events.filter((event) => {
+    if (filter === "All") return true;
+    if (filter === "Deals") return event.ticket_type === "Deals";
+    if (filter === "This_month") return event.show_date === currentMonth;
+    if (filter === "Educational") return event.ticket_type === "Educational";
+    return false;
+  });
+
+  return (
+    <div>
+      <div className="clothe-card-container">
+        {filteredEvents.length > 0 ? (
+          filteredEvents.map((event) => (
+            <div className="show-card" key={event.id}>
+              {event.show_date === currentMonth ? (
+                <div className="Star-function">
+                  <p>{event.show_date}</p>
+                </div>
+              ) : (
+                <div></div>
+              )}
+
+              {event.image && (
+                <img
+                  src={`data:image/jpeg;base64,${event.image}`}
+                  alt={event.title}
+                />
+              )}
+              <div className="showcard-event-details">
+                {event.ticket_type == "Deals" ? (
+                  <h4 className="event-title">{event.title}-Deals</h4>
+                ) : (
+                  <h4 className="event-title">{event.title} </h4>
+                )}
+                <div style={{ display: "flex", gap: "5px", marginTop: "8px" }}>
+                  <i class="fa-solid fa-calendar-days"></i>
+                  <p className="event-date_time">{event.date_time}</p>
+                </div>
+
+                <div style={{ display: "flex", gap: "5px", marginTop: "6px" }}>
+                  <i class="fa-solid fa-location-dot"></i>
+                  <p className="event-venue">{event.venue}</p>
+                </div>
+
+                {event.ticket_type === "Deals" ? (
+                  <div>
+                    <p className="event-ticket_price">
+                      <strong className="highlight">
+                        {event.ticket_price.toFixed(2) - 1000} LKR
+                      </strong>
+                      <span
+                        style={{
+                          textDecoration: "line-through",
+                          marginRight: "8px",
+                          marginLeft: "4px",
+                        }}
+                      >
+                        {event.ticket_price.toFixed(2)} LKR
+                      </span>
+                    </p>
+                  </div>
+                ) : (
+                  <div>
+                    <p className="event-ticket_price">
+                      <strong className="highlight">
+                        {event.ticket_price.toFixed(2)} LKR{" "}
+                      </strong>
+                      upwards
+                    </p>
+                  </div>
+                )}
+
+                {/* <div className="Star-function">
+                  <p>{event.show_date}</p>
+                </div> */}
+              </div>
+
+              {event.available_seats > 0 ? (
+                <button
+                  onClick={() => GotoTicketBuy(event.id)}
+                  className="Buy-ticket-button"
+                >
+                  Buy ticket
+                </button>
+              ) : (
+                <button
+                  onClick={() => GotoTicketBuy(event.id)}
+                  className="sold-out-button"
+                >
+                  sold Out
+                </button>
+              )}
+
+              {event.ticket_type === "Deals" ? (
+                <div className="deal-card"></div>
+              ) : (
+                <div></div>
+              )}
+            </div>
+          ))
+        ) : (
+          <div className="no-event-div">
+            <img src="./src/Images/no events.png" />
+            <p>No events available.</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ShowCardAllEvents;
